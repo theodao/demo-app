ff67868e2ab532b2deac5c8053dbbb52dbc8a4c9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-16 05:54:57 +0000

    Deploy trungdao912/demo-app to github.com/trungdao912/demo-app.git:gh-pages

---
+++ precache-manifest.7020a72775957bce13fb5f88925db476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc02135909c9e08406741ac67e319b48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c12bf84b581be2e4bbb9",
+    "url": "/static/css/main.2492a59b.chunk.css"
+  },
+  {
+    "revision": "48b328f70f2c56c5cc47",
+    "url": "/static/js/2.76df81a6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.76df81a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12bf84b581be2e4bbb9",
+    "url": "/static/js/main.76c972f3.chunk.js"
+  },
+  {
+    "revision": "baeab417597914e097f1",
+    "url": "/static/js/runtime-main.7a4863e1.js"
+  }
+]);
